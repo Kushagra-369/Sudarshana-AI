65314a81571019eada7e1bf029d658a4c2bb370d
Kushagra <knowledge4040god@gmail.com>
2026-09-08 22:40:14 +0530

    ai

--- frontend/src/GlobalAPIURL.tsx
+++ frontend/src/GlobalAPIURL.tsx
@@ -4,4 +4,4 @@ export const APIURL =
   hostname === "localhost" ||
   hostname === "127.0.0.1"
     ? "http://localhost:4321"
-    : "https://sudarshana-ai.onrender.com/";
+    : "https://sudarshana-ai.onrender.com";
